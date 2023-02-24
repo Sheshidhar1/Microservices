68999744903188ac2ee5cee02aa207e9f22371f6
Sheshidhar1 <Sheshidhars1@gmail.com>
2023-02-24 15:32:33 -0700

    New Services

---
+++ microservices/rating-data-service/src/main/java/io/javabrains/ratingdataservice/RatingDataServiceApplication.java
@@ -0,0 +1,15 @@
+package io.javabrains.ratingdataservice;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import  org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+ class RatingDataServiceApplication {
+
+
+	public static void main(String[] args){
+		SpringApplication.run(RatingDataServiceApplication.class, args);
+	}
+}
---
+++ microservices/rating-data-service/src/main/java/io/javabrains/ratingdataservice/RatingsResource.java
@@ -0,0 +1,19 @@
+package io.javabrains.ratingdataservice;
+
+
+import io.javabrains.ratingdataservice.models.Rating;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/ratingsdata")
+public class RatingsResource {
+
+    @RequestMapping("/movies/{movieId}")
+    public Rating getMovieRating(@PathVariable("movieId") String movieId) {
+
+        return new Rating(movieId, 4);
+    }
+}
